d19ab367e6ce9997acb785e4e2c36a847a3523b8
gaurang.semicolon <gaurang.semicolon@gmail.com>
2021-11-22 16:36:07 +0530

    fix issue

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Reader from 'containers/Reader'
 // 60d45480d1f4892d14d7b775/pdf/1634964960315.epub
-// let data = window.location.pathname
-// let first = data.split("/")[2]
-// data = data.split("/")[4]
-// console.log("data, ",data);
-const EPUB_URL = `https://ebook12.s3.eu-central-1.amazonaws.com/60d45480d1f4892d14d7b775/pdf/1634964960315.epub`;
-ReactDOM.render(<Reader url={EPUB_URL} />,  document.getElementById('root'));
+let data = window.location.pathname
+let first = data.split("/")[2]
+data = data.split("/")[4]
+console.log(first,"data, ",data);
+const EPUB_URL = `https://ebook12.s3.eu-central-1.amazonaws.com/${first}/pdf/${data}.epub`;
+ReactDOM.render(<Reader url={EPUB_URL} />,  document.getElementById('root'));   
