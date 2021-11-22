ac45200c7fcf5157bcfa2a4506d04dce20c0379c
gaurang.semicolon <gaurang.semicolon@gmail.com>
2021-11-22 14:40:00 +0530

    change

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Reader from 'containers/Reader'
-
-const EPUB_URL = "/react-epub-viewer/files/Alices Adventures in Wonderland.epub";
-
+// 60d45480d1f4892d14d7b775/pdf/1634964960315.epub
+let data = window.location.pathname
+let first = data.split("/")[2]
+data = data.split("/")[4]
+console.log("data, ",data);
+const EPUB_URL = `https://ebook12.s3.eu-central-1.amazonaws.com/${first}/pdf/${data}`;
 ReactDOM.render(<Reader url={EPUB_URL} />,  document.getElementById('root'));
