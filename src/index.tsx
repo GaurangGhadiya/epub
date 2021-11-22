e2af2bb5b36ac7523152f9a1a500d4aefab16d3e
gaurang.semicolon <gaurang.semicolon@gmail.com>
2021-11-22 16:55:31 +0530

    fix issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Reader from 'containers/Reader'
+import queryString from "query-string";
 // 60d45480d1f4892d14d7b775/pdf/1634964960315.epub
-let data = window.location.pathname
-let first = data.split("/")[2]
-data = data.split("/")[4]
-console.log(first,"data, ",data);
-const EPUB_URL = `https://ebook12.s3.eu-central-1.amazonaws.com/${first}/pdf/${data}`;
+
+const idValue = queryString.parse(window.location.search);
+console.log("data, ",idValue);
+const EPUB_URL = `https://ebook12.s3.eu-central-1.amazonaws.com/${idValue.first}`;
 console.log("EPUB_URL",EPUB_URL);
 
 ReactDOM.render(<Reader url={EPUB_URL} />,  document.getElementById('root'));   
